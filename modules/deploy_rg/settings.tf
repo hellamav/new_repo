e58057d082dbc16ed46d0b3c64185977a7489016
hellamav <108934645+hellamav@users.noreply.github.com>
2024-01-31 21:55:39 +0000

    Update settings.tf

--- modules/deploy_rg/settings.tf
+++ modules/deploy_rg/settings.tf
@@ -7,10 +7,6 @@ provider "azurerm" {
 
 terraform {
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.69.0"
